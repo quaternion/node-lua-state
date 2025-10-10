fdba724e02cecc0fa0dbfd32717b8e42b3859f35
Andrei Nikolaev <qpkskynet@gmail.com>
2025-10-10 20:16:28 +0300

    feat: add esm support

---
+++ js/index.mjs
@@ -0,0 +1,5 @@
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const cjsExports = require("./index.js");
+export default cjsExports;
+export const { LuaState, LuaError } = cjsExports;
