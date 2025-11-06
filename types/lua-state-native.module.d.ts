ee0696f2dfc7e90a24f0ebce5e0dcb878dfca698
Andrei Nikolaev <qpkskynet@gmail.com>
2025-11-06 19:06:49 +0300

    fix(ts): add bigint type to LuaValue union

--- types/lua-state-native.module.d.ts
+++ types/lua-state-native.module.d.ts
@@ -34,7 +34,7 @@ declare module '*lua-state.node' {
     | 'utf8'
 
   export type LuaValue = LuaPrimitive | LuaTable | LuaFunction | LuaValue[]
-  export type LuaPrimitive = string | number | boolean | Date
+  export type LuaPrimitive = string | number | boolean | Date | bigint
   export type LuaFunction = (...args: LuaValue[]) => LuaValue | undefined
   export type LuaTable = {
     [index: string]: LuaValue | undefined
