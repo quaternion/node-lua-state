fd99b620a0907b0b4cee05f42f11a6cc392f3a93
Andrei Nikolaev <qpkskynet@gmail.com>
2026-02-04 10:32:27 +0300

    refactor(lua-error): fix variable naming for code consistency

--- src/lua-error.cpp
+++ src/lua-error.cpp
@@ -29,15 +29,15 @@ LuaError::LuaError(const Napi::CallbackInfo& info) : Napi::ObjectWrap<LuaError>(
   Napi::Object self = info.This().As<Napi::Object>();
   self.Set("name", "LuaError");
 
-  Napi::String message_val = info.Length() > 0 ? info[0].As<Napi::String>() : Napi::String::New(env, "");
-  self.Set("message", message_val);
+  Napi::String message_value = info.Length() > 0 ? info[0].As<Napi::String>() : Napi::String::New(env, "");
+  self.Set("message", message_value);
 
   if (info.Length() > 1 && info[1].IsObject()) {
     Napi::Object options = info[1].As<Napi::Object>();
 
-    auto cause_val = options.Get("cause");
-    if (!cause_val.IsUndefined()) {
-      self.Set("cause", cause_val);
+    auto cause_value = options.Get("cause");
+    if (!cause_value.IsUndefined()) {
+      self.Set("cause", cause_value);
     }
   }
 }
@@ -46,27 +46,29 @@ LuaError::LuaError(const Napi::CallbackInfo& info) : Napi::ObjectWrap<LuaError>(
  * Factory
  */
 Napi::Error LuaError::New(Napi::Env env, const Napi::Object& lua_error) {
-  auto message_val = lua_error.Get("message");
-  auto cause_val = lua_error.Get("cause");
-  auto stack_val = lua_error.Get("stack");
+  auto message_value = lua_error.Get("message");
+  auto cause_value = lua_error.Get("cause");
+  auto stack_value = lua_error.Get("stack");
 
-  auto message = message_val.IsString() ? message_val.As<Napi::String>() : message_val.IsUndefined() ? Napi::String::New(env, "") : message_val.ToString();
+  auto message = message_value.IsString()      ? message_value.As<Napi::String>()
+                 : message_value.IsUndefined() ? Napi::String::New(env, "")
+                                               : message_value.ToString();
 
   Napi::Object options = Napi::Object::New(env);
-  if (!cause_val.IsUndefined()) {
-    options.Set("cause", cause_val);
+  if (!cause_value.IsUndefined()) {
+    options.Set("cause", cause_value);
   }
 
   Napi::Object instance = constructor_.New({message, options});
 
-  if (stack_val.IsString()) {
+  if (stack_value.IsString()) {
     std::string msg = message.Utf8Value();
     std::string header = "LuaError:";
     if (!msg.empty()) {
       header += " " + msg;
     }
 
-    std::string stack = header + "\n    " + stack_val.As<Napi::String>().Utf8Value();
+    std::string stack = header + "\n    " + stack_value.As<Napi::String>().Utf8Value();
 
     instance.Set("stack", stack);
   }
